8cf3ff5b370fb493f9989208854a86ed050dfb54
Karim Hassan <64778200+Karim-W@users.noreply.github.com>
2020-10-31 15:30:20 +0400

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  String _email, _password; //private
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        iconTheme: IconThemeData(color: Colors.white),
+        elevation: 0.0,
+        title: Text(
+          'sign-in',
+          style: TextStyle(
+              fontSize: 24, fontWeight: FontWeight.bold, color: Colors.black),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(25.0),
+        child: Form(
+          key: _formKey,
+          child: Column(children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextFormField(
+                validator: (input) {
+                  if (input.isEmpty) {
+                    return 'field is left empty, please input a vaild email';
+                  }
+                },
+                onSaved: (input) => _email = input,
+                decoration: InputDecoration(labelText: 'Enter your email'),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextFormField(
+                validator: (input) {
+                  if (input.isEmpty) {
+                    return 'field is left empty, please input a vaild password';
+                  }
+                },
+                onSaved: (input) => _password = input,
+                decoration: InputDecoration(labelText: 'Enter your password'),
+              ),
+            )
+          ]),
+        ),
+      ),
+    );
+  }
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      //   title: 'Flutter Demo',
+      //   theme: ThemeData(
+      //     primarySwatch: Colors.brown,
+      //     visualDensity: VisualDensity.adaptivePlatformDensity,
+      //   ),
+      theme: ThemeData.light(),
+      darkTheme: ThemeData.dark(),
+      home: LoginPage(),
+    );
+  }
+}
