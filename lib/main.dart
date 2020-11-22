35aa051dd8173ece873c586ebb6b2ed9fdab33f4
Karim-W <karim.wael@gmail.com>
2020-11-22 05:57:14 +0400

    auth done

--- lib/main.dart
+++ lib/main.dart
@@ -1,85 +1,50 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'authentication_service.dart';
+import 'home_page.dart';
+import 'sign_in_page.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
+import 'package:provider/provider.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
-class LoginPage extends StatefulWidget {
-  @override
-  _LoginPageState createState() => _LoginPageState();
-}
-
-class _LoginPageState extends State<LoginPage> {
-  String _email, _password; //private
-  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        iconTheme: IconThemeData(color: Colors.white),
-        elevation: 0.0,
-        title: Text(
-          'sign-in',
-          style: TextStyle(
-              fontSize: 24, fontWeight: FontWeight.bold, color: Colors.black),
+    return MultiProvider(
+      providers: [
+        Provider<AuthenticationService>(
+          create: (_) => AuthenticationService(FirebaseAuth.instance),
         ),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(25.0),
-        child: Form(
-          key: _formKey,
-          child: Column(children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: TextFormField(
-                validator: (input) {
-                  if (input.isEmpty) {
-                    return 'field is left empty, please input a vaild email';
-                  }
-                },
-                onSaved: (input) => _email = input,
-                decoration: InputDecoration(labelText: 'Enter your email'),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: TextFormField(
-                validator: (input) {
-                  if (input.isEmpty) {
-                    return 'field is left empty, please input a vaild password';
-                  }
-                },
-                onSaved: (input) => _password = input,
-                decoration: InputDecoration(labelText: 'Enter your password'),
-                obscureText: true,
-              ),
-            ),
-            RaisedButton(
-              onPressed: () {},
-              child: Text('Sign-in'),
-            )
-          ]),
+        StreamProvider(
+          create: (context) =>
+              context.read<AuthenticationService>().authStateChanges,
+        )
+      ],
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
         ),
+        home: AuthenticationWrapper(),
       ),
     );
   }
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class AuthenticationWrapper extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      //   title: 'Flutter Demo',
-      //   theme: ThemeData(
-      //     primarySwatch: Colors.brown,
-      //     visualDensity: VisualDensity.adaptivePlatformDensity,
-      //   ),
-      theme: ThemeData.light(),
-      darkTheme: ThemeData.dark(),
-      home: LoginPage(),
-    );
+    final firebaseUser = context.watch<User>();
+
+    if (firebaseUser != null) {
+      return HomePage();
+    }
+    return SignInPage();
   }
 }
