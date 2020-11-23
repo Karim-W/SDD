e2a09d4d84137ac75f66e9363d55806e99944cec
Karim Hassan <Karim.wael@gmail.com>
2020-11-23 08:14:34 +0400

    grids

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,3 +1,5 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/rendering.dart';
 import 'package:playground/Models/manager.dart';
 
@@ -6,6 +8,8 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 class HomePage extends StatefulWidget {
+  HomePage({this.app});
+  final FirebaseApp app;
   @override
   State<StatefulWidget> createState() {
     return _myHomePageState();
@@ -13,27 +17,82 @@ class HomePage extends StatefulWidget {
 }
 
 class _myHomePageState extends State<HomePage> {
+  final gee = ['hello', 'my', 'name', 'is', 'karim'];
+  List<int> myList = List<int>();
+  //myList.add(42);
+  final databaseReference = FirebaseDatabase.instance;
   @override
   Widget build(BuildContextcontext) {
+    final DBref = databaseReference.reference();
     return new Scaffold(
-        appBar: new AppBar(
-          title: Text('Hello'),
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text("HOME"),
-              RaisedButton(
-                onPressed: () {
-                  context.read<AuthenticationService>().signOut();
-                },
-                child: Text("Sign out"),
-              ),
-            ],
+      backgroundColor: Colors.grey,
+      appBar: new AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        leading: Icon(Icons.menu),
+        title: Text("Home"),
+        actions: <Widget>[
+          FlatButton(
+            textColor: Colors.white,
+            onPressed: () {
+              context.read<AuthenticationService>().signOut();
+            },
+            child: Text("Logout"),
+            shape: CircleBorder(side: BorderSide(color: Colors.transparent)),
           ),
-        ));
+        ],
+      ),
+      body: Container(
+        child: _buildHorizontalList(parentIndex: 1, howmany: 5),
+      ),
+    );
   }
+}
+
+Widget _buildHorizontalList({int parentIndex, int howmany}) {
+  var colors = [
+    Colors.green,
+    Colors.blue,
+    Colors.indigo,
+    Colors.red,
+    Colors.orange
+  ];
+  double height = 250.0;
+  return SizedBox(
+    height: height,
+    child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: howmany,
+        itemBuilder: (BuildContext content, int index) {
+          return _buildItem(
+              index: index + 1,
+              color: colors[(parentIndex + index) % colors.length],
+              parentSize: height,
+              name: "null");
+        }),
+  );
+}
+
+Widget _buildItem({int index, Color color, double parentSize, String name}) {
+  double edgeSize = 8.0;
+  double itemSize = parentSize - edgeSize * 2.0;
+  return Container(
+    padding: EdgeInsets.all(edgeSize),
+    child: SizedBox(
+      width: itemSize,
+      height: itemSize,
+      child: Padding(
+        padding: EdgeInsets.all(0.0),
+        child: Container(
+          width: 36,
+          height: 36,
+          decoration: BoxDecoration(
+              color: color, borderRadius: BorderRadius.circular(10)),
+          child: Center(child: Text(name)),
+        ),
+      ),
+    ),
+  );
 }
 
 // class HomePage extends StatelessWidget {
