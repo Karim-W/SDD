6d0f7796aa83a015f40ce5ce03c334af88f8558b
Karim Hassan <Karim.wael@gmail.com>
2020-11-25 20:48:49 +0400

    Home UI donee

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -2,6 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/rendering.dart';
 import 'package:playground/Models/manager.dart';
+import 'package:playground/Models/violation.dart';
 
 import 'authentication_service.dart';
 import 'package:flutter/material.dart';
@@ -18,62 +19,104 @@ class HomePage extends StatefulWidget {
 
 class _myHomePageState extends State<HomePage> {
   final gee = ['hello', 'my', 'name', 'is', 'karim'];
-  List<int> myList = List<int>();
-  //myList.add(42);
+  List<String> intArr = ['Paul', 'Carrefour', 'FedEx', 'Nike', 'Nandos'];
+  List<String> locArr = ['Mirdif', 'Downtown', 'Sports City', 'JBR', 'Marina'];
+  List<String> cityArr = [
+    'Dubai,UAE',
+    'Dubai,UAE',
+    'Dubai,UAE',
+    'Dubai,UAE',
+    'Dubai,UAE'
+  ];
+  List<int> vioArr = [3, 5, 2, 1, 0];
   final databaseReference = FirebaseDatabase.instance;
   @override
   Widget build(BuildContextcontext) {
     final DBref = databaseReference.reference();
     return new Scaffold(
-      backgroundColor: Colors.grey,
-      appBar: new AppBar(
-        backgroundColor: Colors.transparent,
-        elevation: 0,
-        leading: Icon(Icons.menu),
-        title: Text("Home"),
-        actions: <Widget>[
-          FlatButton(
-            textColor: Colors.white,
-            onPressed: () {
-              context.read<AuthenticationService>().signOut();
-            },
-            child: Text("Logout"),
-            shape: CircleBorder(side: BorderSide(color: Colors.transparent)),
+        backgroundColor: Color(0xff0B0500),
+        appBar: new AppBar(
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+          leading: Icon(Icons.menu),
+          title: Text("Home"),
+          actions: <Widget>[
+            FlatButton(
+              textColor: Colors.white,
+              onPressed: () {
+                context.read<AuthenticationService>().signOut();
+              },
+              child: Text("Logout"),
+              shape: CircleBorder(side: BorderSide(color: Colors.transparent)),
+            ),
+          ],
+        ),
+        body: Padding(
+          padding: EdgeInsets.only(top: 16.0),
+          child: Column(
+            children: [
+              Container(
+                child: _buildHorizontalList(
+                    w_idth: MediaQuery.of(context).size.width,
+                    parentIndex: 1,
+                    howmany: intArr.length,
+                    locTitle: intArr,
+                    locArea: locArr,
+                    locCity: cityArr,
+                    vioArr: vioArr),
+              ),
+              Padding(
+                  padding: EdgeInsets.all(16.0),
+                  child: Container(
+                      alignment: Alignment.topLeft,
+                      child: Text(
+                        "HELooooo",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold),
+                      ))),
+            ],
           ),
-        ],
-      ),
-      body: Container(
-        child: _buildHorizontalList(parentIndex: 1, howmany: 5),
-      ),
-    );
+        ));
   }
 }
 
-Widget _buildHorizontalList({int parentIndex, int howmany}) {
-  var colors = [
-    Colors.green,
-    Colors.blue,
-    Colors.indigo,
-    Colors.red,
-    Colors.orange
-  ];
-  double height = 250.0;
+Widget _buildHorizontalList(
+    {double w_idth,
+    int parentIndex,
+    int howmany,
+    List<String> locTitle,
+    List<String> locArea,
+    List<String> locCity,
+    List<int> vioArr}) {
+  double height = w_idth - 2;
   return SizedBox(
-    height: height,
+    height: height * 1.6,
     child: ListView.builder(
         scrollDirection: Axis.horizontal,
         itemCount: howmany,
         itemBuilder: (BuildContext content, int index) {
           return _buildItem(
               index: index + 1,
-              color: colors[(parentIndex + index) % colors.length],
+              color: Colors.deepPurpleAccent,
               parentSize: height,
-              name: "null");
+              name: locTitle[index],
+              area: locArea[index],
+              city: locCity[index],
+              violations: vioArr[index]);
         }),
   );
 }
 
-Widget _buildItem({int index, Color color, double parentSize, String name}) {
+Widget _buildItem(
+    {int index,
+    Color color,
+    double parentSize,
+    String name,
+    String area,
+    String city,
+    int violations}) {
   double edgeSize = 8.0;
   double itemSize = parentSize - edgeSize * 2.0;
   return Container(
@@ -87,8 +130,35 @@ Widget _buildItem({int index, Color color, double parentSize, String name}) {
           width: 36,
           height: 36,
           decoration: BoxDecoration(
-              color: color, borderRadius: BorderRadius.circular(10)),
-          child: Center(child: Text(name)),
+              color: color,
+              gradient: new LinearGradient(
+                  colors: [Colors.purple, color.withOpacity(0.75)],
+                  begin: Alignment.centerRight,
+                  end: new Alignment(-1.0, -1.0)),
+              borderRadius: BorderRadius.circular(10)),
+          child: Container(
+              alignment: Alignment.bottomCenter, //.bottomCenter,
+              child: Padding(
+                  padding: EdgeInsets.only(top: (itemSize * 1.4) - 16),
+                  child: Column(children: [
+                    Text(
+                      name,
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    Text(area + "," + city,
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                        )),
+                    Text("Number of Violations: " + violations.toString(),
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                        ))
+                  ]))),
         ),
       ),
     ),
